4097af3236176d920b67a2a362a7e6faa13a2f5e
mzyao <imzyao@163.com>
2025-07-15 21:35:26 +0800

    feat：搜索

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     ignoreDeadLinks: true,
     head: [
         ['link', {rel: 'icon', href: '/vite-coding-doc/favicon.ico'}],
-        ['meta', { name: 'algolia-site-verification', content: '1EF515598795DEB3' }],
+        ['meta', {name: 'algolia-site-verification', content: '1EF515598795DEB3'}],
     ],
     markdown: {
         //行号显示
@@ -41,6 +41,58 @@ export default defineConfig({
         docFooter: {
             prev: '上一页',
             next: '下一页',
+        },
+        //Algolia搜索纯中文版
+        search: {
+            provider: 'algolia',
+            options: {
+                appId: 'N4AJT5RFWW',
+                apiKey: '58bb2ceb7a7deb71027ed2e56661092b',
+                indexName: 'My-Vite-Doc',
+                locales: {
+                    root: {
+                        placeholder: '搜索文档',
+                        translations: {
+                            button: {
+                                buttonText: '搜索文档',
+                                buttonAriaLabel: '搜索文档'
+                            },
+                            modal: {
+                                searchBox: {
+                                    resetButtonTitle: '清除查询条件',
+                                    resetButtonAriaLabel: '清除查询条件',
+                                    cancelButtonText: '取消',
+                                    cancelButtonAriaLabel: '取消'
+                                },
+                                startScreen: {
+                                    recentSearchesTitle: '搜索历史',
+                                    noRecentSearchesText: '没有搜索历史',
+                                    saveRecentSearchButtonTitle: '保存至搜索历史',
+                                    removeRecentSearchButtonTitle: '从搜索历史中移除',
+                                    favoriteSearchesTitle: '收藏',
+                                    removeFavoriteSearchButtonTitle: '从收藏中移除'
+                                },
+                                errorScreen: {
+                                    titleText: '无法获取结果',
+                                    helpText: '你可能需要检查你的网络连接'
+                                },
+                                footer: {
+                                    selectText: '选择',
+                                    navigateText: '切换',
+                                    closeText: '关闭',
+                                    searchByText: '搜索提供者'
+                                },
+                                noResultsScreen: {
+                                    noResultsText: '无法找到相关结果',
+                                    suggestedQueryText: '你可以尝试查询',
+                                    reportMissingResultsText: '你认为该查询应该有结果？',
+                                    reportMissingResultsLinkText: '点击反馈'
+                                },
+                            },
+                        },
+                    },
+                },
+            },
         },
         sidebar: [
             {
